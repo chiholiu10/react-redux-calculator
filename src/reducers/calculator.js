4efef060d361f2be6bc1f5951b1dfba4d16c4984
Chiho Liu <>
2019-09-09 10:48:12 +0200

    reducers conflict solved

--- src/reducers/calculator.js
+++ src/reducers/calculator.js
@@ -2,13 +2,9 @@ import { types } from '../actions/index';
 
 const initialState = {
     value: 0,
-<<<<<<< HEAD
     history: 0,
     comma: [","],
     toggle: false,
-=======
-    history: [],
->>>>>>> b573293b3b76a3427b31b3921a1f8a66f1911d37
     numbers: [ "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"],   
     divide: [ "/" ],
     reset: ["C"],
@@ -22,11 +18,6 @@ const initialState = {
 }
 
 export const calculator = (state = initialState, action) => {
-<<<<<<< HEAD
-
-=======
-    
->>>>>>> b573293b3b76a3427b31b3921a1f8a66f1911d37
     switch (action.type) {
         case types.INPUT_NUMBER:
             console.log(state.value)
@@ -36,17 +27,10 @@ export const calculator = (state = initialState, action) => {
             }
 
         case types.PLUS:
-<<<<<<< HEAD
             console.log(state.value)
             return {
                 ...state,
                 value: state.value
-=======
-            console.log(state.value);
-            return {
-                ...state,
-                value: state.value.concat(action.plus)
->>>>>>> b573293b3b76a3427b31b3921a1f8a66f1911d37
             }
         case types.DIVIDE:
             return {
